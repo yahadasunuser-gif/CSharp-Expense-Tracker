f3a17cdbd394505a2f94ea7f197b74e0c8681a3a
Yahadasun_Prabashwara <yahadasunuser@gmail.com>
2026-04-13 15:47:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿decimal budget;
+decimal remainingBudget;
+string expenseName;
+decimal expense;
+while (true)
+{
+    Console.WriteLine("Enter Total budget: ");
+    string stringBudget = Console.ReadLine();
+    budget = decimal.Parse(stringBudget);
+    if (budget < 0)
+    {
+        Console.WriteLine("Please enter a positive number");
+    }
+    else if (budget == 0)
+    {
+        Console.WriteLine("Please enter a number greater than zero");
+    }
+    else 
+    {
+        break;
+    }
+}
+while (true)
+    {
+        Console.WriteLine("Enter you expenxce name: ");
+        Console.WriteLine("Enter you expenxce value: ");
+        expenseName = Console.ReadLine();
+        string expensestring = Console.ReadLine();
+        expense = decimal.Parse(expensestring);
+        
+        if (expense < 0)
+        {
+            Console.WriteLine("Please enter a positive number");
+        }
+        else if (expense == 0)
+        {
+            Console.WriteLine("Please enter a number greater than zero");
+        }
+        else 
+        {
+            break;
+        }
+        
+    }
+   
+
